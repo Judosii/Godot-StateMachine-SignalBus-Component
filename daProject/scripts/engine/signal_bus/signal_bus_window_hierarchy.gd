c0f66e1d47cc15b62378d61007557885c0d2ff31
Judosii <skutnik.martin@gmail.com>
2026-01-22 21:08:02 +0100

    fancy ish buttons on signal broker

--- daProject/scripts/engine/signal_bus/signal_bus_window_hierarchy.gd
+++ daProject/scripts/engine/signal_bus/signal_bus_window_hierarchy.gd
@@ -3,6 +3,10 @@ extends Control
 class_name SignalBus_WindowHierarchy
 
 @export var nodeList: VBoxContainer
+@export var buttonHoverSize: Vector2
+@export var buttonPressSize: Vector2
+
+var nodeInListSelected: Button
 
 func _ready() -> void:
 	print("signal bus window hierarchy instanced!")
@@ -10,15 +14,38 @@ func _ready() -> void:
 func GetNodeList()-> VBoxContainer: #WARNING, child 0 will always be title
 	return nodeList
 
-func MakeNewNodeEntry(_n: Node) -> HBoxContainer:
-	var _entryParent:= HBoxContainer.new()
-	var _entryIcon:= TextureRect.new()
-	var _entryName:= Label.new()
+func MakeNewNodeEntry(_n: Node) -> Control:
+	var _button:= Button.new()
 	
-	_entryParent.add_child(_entryIcon)
-	_entryParent.add_child(_entryName)
-	_entryName.text = _n.name
-	_entryIcon.texture = EditorInterface.get_editor_theme().get_icon(_n.get_class(), &"EditorIcons")
-	_entryIcon.stretch_mode = TextureRect.STRETCH_KEEP
+	var _cEnter:= Callable(self, "ButtonMouseEntered").bind(_button)
+	_button.mouse_entered.connect(_cEnter)
+	var _cExit:= Callable(self, "ButtonMouseExited").bind(_button)
+	_button.mouse_exited.connect(_cExit)
+	var _cPress:= Callable(self, "ButtonMousePressed").bind(_button)
+	_button.pressed.connect(_cPress)
 	
-	return _entryParent
+	_button.icon = EditorInterface.get_editor_theme().get_icon(_n.get_class(), &"EditorIcons")
+	_button.expand_icon = true
+	_button.text = _n.name
+	_button.connect("pressed", Callable(self, "NewNodeSelected"))
+	var _cInit := Callable(self, "InitializeButton").bind(_button)
+	_cInit.call_deferred()
+	return _button
+
+#region NodeEntry Button Stuff
+func InitializeButton(_b:Button):
+	_b.pivot_offset = _b.size/2.0
+
+func ButtonMouseEntered(_b:Button):
+	create_tween().tween_property(_b,"scale",buttonHoverSize,0.1).set_trans(Tween.TRANS_SINE)
+
+func ButtonMouseExited(_b:Button):
+	create_tween().tween_property(_b,"scale",Vector2.ONE,0.1).set_trans(Tween.TRANS_SINE)
+
+func ButtonMousePressed(_b:Button):
+	create_tween().tween_property(_b,"scale",buttonPressSize,0.06).set_trans(Tween.TRANS_SINE)
+	create_tween().tween_property(_b,"scale", buttonHoverSize, 0.2).set_trans(Tween.TRANS_SINE)
+#endregion
+
+func NewNodeSelected():
+	print("a")
